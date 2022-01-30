b39c7ca4295318d232e132558b6a6b45081fe618
Alekiel42 <alexandra.bercoff@gmail.com>
2022-01-30 13:59:34 +0100

    improve character details css

--- stargate/characters/views.py
+++ stargate/characters/views.py
@@ -6,7 +6,7 @@ characters_info = [
         "image": "jack.jpg",
         "name": "Jack O'Neill",
         "planet": "Terre",
-        "excerpt": "Jack est un colonel de talent et rebel",
+        "excerpt": "Jack O'Neill est un personnage capable de plaisanter dans les pires moments.",
         "citation": "O'Neill... avec deux l",
         "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, consequuntur modi. Debitis accusamus sit id. Cum consequatur ullam reiciendis laboriosam, eius illum iure quia, inventore tempore accusamus accusantium reprehenderit necessitatibus deserunt dolore! Quidem ipsa rerum consectetur temporibus dolor doloribus, officiis est eligendi reiciendis accusantium sed mollitia esse nam suscipit aperiam maxime placeat illum vel aliquid? Inventore enim, officia, doloremque minus itaque commodi, incidunt dolorum tempora libero possimus assumenda eveniet asperiores."
     },
@@ -15,7 +15,7 @@ characters_info = [
         "image": "samantha.jpg",
         "name": "Samantha Carter",
          "planet": "Terre",
-        "citation": "je suis sam",
+        "citation": "Je suis officier de l'armée de l'air, comme vous, mon colonel. Et le fait que mes organes de reproduction soient situés à l'intérieur de mon corps, ne veut pas dire que je vous suis inférieure, mon colonel...",
         "excerpt": "Samantha est une scientifique soldat hors pair",
         "description": "Smantha Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, consequuntur modi. Debitis accusamus sit id. Cum consequatur ullam reiciendis laboriosam, eius illum iure quia, inventore tempore accusamus accusantium reprehenderit necessitatibus deserunt dolore! Quidem ipsa rerum consectetur temporibus dolor doloribus, officiis est eligendi reiciendis accusantium sed mollitia esse nam suscipit aperiam maxime placeat illum vel aliquid? Inventore enim, officia, doloremque minus itaque commodi, incidunt dolorum tempora libero possimus assumenda eveniet asperiores."
     },
@@ -24,7 +24,7 @@ characters_info = [
         "image": "daniel.jpg",
         "name": "Daniel Jackson",
          "planet": "Terre",
-        "citation": "blabla egypte",
+        "citation": "Ça ne vous est jamais arrivé de ressentir quelque chose de totalement contraire à la logique, et qui s'avère pourtant exact ?",
         "excerpt": "Egyptologue, polyglotte et en pleine réflexion",
         "description": "Daniel Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, consequuntur modi. Debitis accusamus sit id. Cum consequatur ullam reiciendis laboriosam, eius illum iure quia, inventore tempore accusamus accusantium reprehenderit necessitatibus deserunt dolore! Quidem ipsa rerum consectetur temporibus dolor doloribus, officiis est eligendi reiciendis accusantium sed mollitia esse nam suscipit aperiam maxime placeat illum vel aliquid? Inventore enim, officia, doloremque minus itaque commodi, incidunt dolorum tempora libero possimus assumenda eveniet asperiores."
     }
